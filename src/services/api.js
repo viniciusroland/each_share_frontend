25666c79e764a5eaf76ddf9a22d4fc4a9be39224
Vinicius Roland <vinicius.roland@btgpactual.com>
2020-03-26 15:14:23 -0300

    adding base url from deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const api = axios.create({
-  baseURL: "http://localhost:4000"
+  //baseURL: "http://localhost:4000"
+  baseURL: "http://142.93.11.203:4000"
 })
 
 export default api
